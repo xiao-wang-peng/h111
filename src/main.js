26618c1048e1ac8b531d85a3df53d73bbe2574d4
xiao_wang_peng <5665754+wang_peng_12@user.noreply.gitee.com>
2020-10-25 17:19:36 +0800

    完成文件基本结构引入element

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "./style/index.less";
+
+// 引入组件 element-ui
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+/* eslint-disable semi */
+<template>
+  <div>
+    <h1>我是login页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+};
+</script>
+<style lang="" scoped></style>
