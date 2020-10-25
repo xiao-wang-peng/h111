64dc3f008e7c9ed65ecaf48128dbbf3a03d752d0
xiao_wang_peng <5665754+wang_peng_12@user.noreply.gitee.com>
2020-10-25 20:57:13 +0800

    添加了正则

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -6,29 +6,29 @@
         <div class="logo"></div>
       </div>
       <!-- 表单区域 -->
-      <el-form class="login-form" ref="form">
-        <el-form-item>
-          <el-input v-model="name" placeholder="请输入账号"></el-input>
+      <el-form class="login-form" ref="form" :rules="rules" :model="form">
+        <el-form-item prop="name">
+          <el-input v-model="form.name" placeholder="请输入账号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
-            v-model="password"
+            v-model="form.password"
             placeholder="请输入密码"
             show-password
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="isNum">
           <el-checkbox
             label="我已阅读并同意用户协议和隐私条款"
             name="type"
-            v-model="isNum"
+            v-model="form.isNum"
           ></el-checkbox>
         </el-form-item>
         <el-form-item>
           <el-button
             class="login-btn"
             :loading="isLoading"
-            :disabled="!isNum"
+            :disabled="!form.isNum"
             type="primary"
             @click.prevent="deng"
             >登陆</el-button
@@ -46,12 +46,49 @@ export default {
   props: [""],
   data() {
     return {
-      // 用户账号
-      name: "",
-      // 用户密码
-      password: "",
-      // 是否勾选协议
-      isNum: false,
+      rules: {
+        name: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          {
+            pattern: /^1\d{10}$/,
+            message: "请输入正确的手机号",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          { pattern: /^\d{6}$/, message: "请输入正确的验证码", trigger: "blur" }
+        ],
+        isNum: [
+          {
+            validator: (rule, value, callback) => {
+              // console.log('是否同意', value)
+              // 如果检验通过 ，直接执行callback
+              if (value) {
+                callback();
+              } else {
+                callback(new Error("你必须同意，才能登陆"));
+              }
+              // if (value === '') {
+              //   callback(new Error('请再次输入密码'));
+              // } else if (value !== this.ruleForm2.pass) {
+              //   callback(new Error('两次输入密码不一致!'));
+              // } else {
+              //   callback();
+              // }
+            },
+            trigger: "change"
+          }
+        ]
+      },
+      form: {
+        // 用户账号
+        name: "13911111111",
+        // 用户密码
+        password: "246810",
+        // 是否勾选协议
+        isNum: false
+      },
       // 判断按钮是否处于加载中
       isLoading: false
     };
@@ -68,7 +105,7 @@ export default {
   methods: {
     deng() {
       // es6解构获取的账号和密码
-      const { name, password } = this;
+      const { name, password } = this.form;
       //  判断用户是否未输入就登陆
       if (name === "" || password === "") return;
       // 点击之后切换按钮状态 改变为加载中
