f5147359d9f0221f7d4884ffdcdaf1d62aab5775
xiao_wang_peng <5665754+wang_peng_12@user.noreply.gitee.com>
2020-10-30 20:56:18 +0800

    上传图片-添加收藏或取消

--- src/views/images/image.vue
+++ src/views/images/image.vue
@@ -9,6 +9,13 @@
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
+        <el-button
+          @click="dialogVisible = true"
+          type="success"
+          size="mini"
+          style="float:right"
+          >上传图片素材
+        </el-button>
       </div>
       <!-- 素材列表 -->
       <el-row :gutter="10">
@@ -30,8 +37,9 @@
             <span
               class="el-icon-star-off"
               :class="{ red: item.is_collected }"
+              @click="huan(item)"
             ></span>
-            <span class="el-icon-delete"></span>
+            <span class="el-icon-delete" @click="del(item.id)"></span>
           </div>
         </el-col>
       </el-row>
@@ -45,6 +53,39 @@
       </el-pagination>
       <!-- /素材列表 -->
     </el-card>
+    <!--
+      .sync可以用来做双向绑定
+      append-to-body: 让对话框显示在最上层
+    -->
+    <el-dialog title="素材上传" append-to-body :visible.sync="dialogVisible">
+      <!--
+- action： 必选参数，上传的地址：http://ttapi.research.itcast.cn/mp/v1_0/user/images
+- headers: 设置上传的请求头部。要携带token
+- name：上传的文件字段名。要与接口中要求保持一致。
+- show-file-list： 是否显示已上传文件列表
+- on-success：文件上传成功时的钩子。`function(response, file, fileList)`
+- before-upload: 上传之前对文件进行检测。 -->
+      <el-upload
+        class="avatar-uploader"
+        action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+        :headers="headers"
+        name="image"
+        :show-file-list="false"
+        :before-upload="hBeforeUpload"
+        :on-success="hUploadSuccess"
+      >
+        <!-- img用来显示上传成功之后的图像 -->
+        <el-image
+          v-if="imageUrl"
+          :src="imageUrl"
+          class="avatar"
+          @load="hpreviewImageload"
+        ></el-image>
+        <!-- <img v-if="imageUrl" :src="imageUrl" class="avatar"> -->
+        <!-- 显示引导用户上传的 那个 + 号 -->
+        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -54,9 +95,14 @@ export default {
   props: [""],
   data() {
     return {
+      imageUrl: "",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      },
       isCollect: false, // 是否是收藏的图片 true表示是收藏，false表示是全部
       curPage: 1, // 当前查询第几页
       total_count: 0, // 查询的总数量
+      dialogVisible: false, // 是否显示弹出对话框
       images: [] // 素材列表
     };
   },
@@ -73,6 +119,26 @@ export default {
   },
 
   methods: {
+    // 切换收藏或不收藏
+    async huan(item) {
+      const id = item.id;
+      try {
+        const res = await this.$axios({
+          url: "/mp/v1_0/user/images/" + id,
+          method: "PUT",
+          data: {
+            collect: !item.is_collected
+          }
+        });
+
+        this.$message.success("操作成功");
+        item.is_collected = !item.is_collected;
+      } catch (error) {
+        console.log(error);
+        this.$message.error("操作失败");
+      }
+    },
+    // 获取素材信息
     async getList() {
       try {
         const res = await this.$axios({
@@ -91,9 +157,41 @@ export default {
         console.log(err);
       }
     },
+    // 切换分页
     PageChange(page) {
       this.curPage = page;
       this.getList();
+    },
+    hpreviewImageload() {
+      // 表示图片预览成功
+      console.log("图片预览成功", Date.now());
+      setTimeout(() => {
+        this.dialogVisible = false;
+        this.imageUrl = "";
+      }, 2000);
+    },
+    // 上传成功
+    hUploadSuccess(res, file) {
+      // res中保存着本次上传操作成功后，后端接口返回的数值
+      // 1. 预览效果
+      this.imageUrl = res.data.url;
+      // console.log('文件上传成功', res, file)
+
+      // 2. 重新请求一次
+      this.getList();
+    },
+    hBeforeUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      // file.size 单位是字节
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
     },
     // 切换收藏和全部
     qieH() {
